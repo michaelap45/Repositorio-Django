58bf668779a0656375cce86f050cded840390de7
Michael A. Perea <16171530@itculiacan.edu.mx>
2021-08-03 16:28:10 -0600

    configuracion de archivos estaticos

--- miproyecto/boards/tests.py
+++ miproyecto/boards/tests.py
@@ -1,3 +1,15 @@
+from .views import home
 from django.test import TestCase
+from django.urls import reverse
+from django.urls.base import resolve
 
-# Create your tests here.
+#pruebas de home
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+    
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func, home)
--- miproyecto/boards/views.py
+++ miproyecto/boards/views.py
@@ -1,5 +1,7 @@
+from .models import Board
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
