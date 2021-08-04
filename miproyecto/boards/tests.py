d921b34c1f131815f873f84159bedb2769a35309
Michael A. Perea <16171530@itculiacan.edu.mx>
2021-08-03 19:46:34 -0600

    enlaces entre la pantalla home y topic boards

--- miproyecto/boards/tests.py
+++ miproyecto/boards/tests.py
@@ -1,10 +1,17 @@
-from .views import home
+
+from .models import Board
+from .views import board_topics, home
 from django.test import TestCase
 from django.urls import reverse
 from django.urls.base import resolve
 
 #pruebas de home
 class HomeTests(TestCase):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board.')
+        url = reverse('home')
+        self.response = self.client.get(url)
+
     def test_home_view_status_code(self):
         url = reverse('home')
         response = self.client.get(url)
@@ -13,3 +20,32 @@ class HomeTests(TestCase):
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
+    
+    def test_home_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
+
+#pruebas de board topics
+class BoardTopicsTests(TestCase):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board.')
+
+    def test_board_topics_view_success_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': self.board.pk })
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_board_topics_view_not_found_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_board_topics_url_resolves_board_topics_view(self):
+        view = resolve('/boards/1/')
+        self.assertEquals(view.func, board_topics)
+    
+    def test_board_topics_view_contains_link_back_to_homepage(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
+        homepage_url = reverse('home')
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
--- miproyecto/boards/views.py
+++ miproyecto/boards/views.py
@@ -1,7 +1,12 @@
+from django.http.response import Http404
 from .models import Board
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
