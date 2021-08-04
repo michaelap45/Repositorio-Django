80a8be782dc823ba4f1acc830c91cdf29b3469bf
Michael A. Perea <16171530@itculiacan.edu.mx>
2021-08-03 20:46:09 -0600

    plantillas de formularios reutilizables y pruebas para new topic

--- miproyecto/boards/tests.py
+++ miproyecto/boards/tests.py
@@ -1,4 +1,5 @@
 
+from .forms import NewTopicForm
 from django.contrib.auth.models import User
 from .models import Board, Post, Topic
 from .views import board_topics, home, new_topic
@@ -107,7 +108,9 @@ class NewTopicTests(TestCase):
         '''
         url = reverse('new_topic', kwargs={'pk': 1})
         response = self.client.post(url, {})
+        form = response.context.get('form')
         self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
 
     def test_new_topic_invalid_post_data_empty_fields(self):
         '''
@@ -123,3 +126,9 @@ class NewTopicTests(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertFalse(Topic.objects.exists())
         self.assertFalse(Post.objects.exists())
+    
+    def test_contains_form(self):  
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        form = response.context.get('form')
+        self.assertIsInstance(form, NewTopicForm)
