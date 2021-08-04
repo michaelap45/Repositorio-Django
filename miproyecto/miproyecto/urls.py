8718bf0d56793271310b1ff460cf092211a0bbb0
Michael A. Perea <16171530@itculiacan.edu.mx>
2021-08-04 03:34:29 -0600

    diseño para la pagina signup y crear cerrar sesion

--- miproyecto/miproyecto/urls.py
+++ miproyecto/miproyecto/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from boards import views
 from accounts import views as accounts_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', accounts_views.signup, name='signup'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
