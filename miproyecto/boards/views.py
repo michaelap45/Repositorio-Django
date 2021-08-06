e422584604b659977cb3699d1d08d47a0b8e5419
Michael A. Perea <16171530@itculiacan.edu.mx>
2021-08-06 02:28:18 -0600

    refactorizacion de home a CBV

--- miproyecto/boards/views.py
+++ miproyecto/boards/views.py
@@ -5,14 +5,15 @@ from django.shortcuts import get_object_or_404, redirect, render
 from .forms import NewTopicForm, PostForm
 from .models import Board, Post, Topic
 
-from django.views.generic import UpdateView
+from django.views.generic import UpdateView, ListView
 from django.utils import timezone
 from django.utils.decorators import method_decorator
 
 
-def home(request):
-    boards = Board.objects.all()
-    return render(request, 'home.html', {'boards': boards})
+class BoardListView(ListView):
+    model = Board
+    context_object_name = 'boards'
+    template_name = 'home.html'
 
 
 def board_topics(request, pk):
--- miproyecto/miproyecto/urls.py
+++ miproyecto/miproyecto/urls.py
@@ -20,7 +20,7 @@ from accounts import views as accounts_views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.BoardListView.as_view(), name='home'),
     path('signup/', accounts_views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
