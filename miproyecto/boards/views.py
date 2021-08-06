6a26e2465d77a637c46f74efca0542500a99d057
Michael A. Perea <16171530@itculiacan.edu.mx>
2021-08-05 18:23:06 -0600

    inicio de sesion para usuarios no registrados

--- miproyecto/boards/views.py
+++ miproyecto/boards/views.py
@@ -4,6 +4,7 @@ from .models import Board, Post, Topic
 from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     boards = Board.objects.all()
@@ -13,9 +14,10 @@ def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
 
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    user = User.objects.first()  
+    user = User.objects.first()  # TODO: get the currently logged in user
     if request.method == 'POST':
         form = NewTopicForm(request.POST)
         if form.is_valid():
@@ -28,7 +30,7 @@ def new_topic(request, pk):
                 topic=topic,
                 created_by=user
             )
-            return redirect('board_topics', pk=board.pk)
+            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
     else:
         form = NewTopicForm()
     return render(request, 'new_topic.html', {'board': board, 'form': form})
