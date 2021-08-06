1fc8cc82c743e3918b343f00a8f634a59fd51944
Michael A. Perea <16171530@itculiacan.edu.mx>
2021-08-06 01:16:56 -0600

    tests para la vista de actualizacion

--- miproyecto/boards/views.py
+++ miproyecto/boards/views.py
@@ -7,6 +7,7 @@ from .models import Board, Post, Topic
 
 from django.views.generic import UpdateView
 from django.utils import timezone
+from django.utils.decorators import method_decorator
 
 
 def home(request):
@@ -63,12 +64,17 @@ def reply_topic(request, pk, topic_pk):
         form = PostForm()
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
 
+@method_decorator(login_required, name='dispatch')
 class PostUpdateView(UpdateView):
     model = Post
     fields = ('message', )
     template_name = 'edit_post.html'
     pk_url_kwarg = 'post_pk'
     context_object_name = 'post'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(created_by=self.request.user)
 
     def form_valid(self, form):
         post = form.save(commit=False)
